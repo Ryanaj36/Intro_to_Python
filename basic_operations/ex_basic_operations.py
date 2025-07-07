ded21924ca56041ab2facb84fee0112351fcfc8a
Ryanaj36 <Ryanaj36@gmail.com>
2025-07-07 23:33:58 +0000

    All exercises complete on one file.

---
+++ basic_operations/ex_basic_operations.py
@@ -0,0 +1,59 @@
+# Exercise 1
+print('1)')
+print('John' + ' ' + 'Doe')
+print ('')
+
+# Exercise 2
+print ('2)')
+number = 4936
+ones = 4936 % 10
+print(ones)
+
+number = number // 10
+tens = number % 10
+print(tens)
+
+number = number // 10
+hundreds = number % 10
+print(hundreds)
+
+number = number // 10
+thousands = number % 10
+print(thousands)
+
+print('')
+
+# Exercise 3
+print ('3) What does the following code do? Why?')
+print("print('5' + '10')")
+print ('5' + '10')
+
+print('')
+
+#Exercise 4
+print('4)')
+print(int('5') + int('10'))
+
+print('')
+
+#Exercise 5
+print('5)')
+print('yes')
+
+print('')
+
+#Exercise 6
+print('6)')
+print('False')
+
+print('')
+
+#Exercise 7
+print('7)')
+print('Raises a ValueError since the string value 3.1415 does not represent a valid integer. It\'s a float.')
+
+print ('')
+
+#Exercise 8
+print('8)')
+print('True')
